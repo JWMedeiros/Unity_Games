29ac6be597467c651f3a923f6aa5642244e3901b
John Boi <jon0908776@gmail.com>
2023-02-22 01:25:44 -0500

    Update to movement scripts and increased timeline

--- DefendersOfGalactica/Assets/Scripts/PlayerControls.cs
+++ DefendersOfGalactica/Assets/Scripts/PlayerControls.cs
@@ -3,12 +3,30 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
     [SerializeField] float controlSpeed = 20f;
-    [SerializeField] float xRange = 5f;
+    [SerializeField] float xRange = 6f;
     [SerializeField] float yRange = 5f;
+
+    //Changing Pitch with up/down movement
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -25f;
+
+    [SerializeField] float positionYawFactor= 3f;
+
+    //Changing Roll with left/right Movement
+    [SerializeField] float positionRollFactor = -2f;
+    [SerializeField] float controlRollFactor = -10f;
+
+    float horizontalThrow, verticalThrow;
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
-        float verticalThrow = Input.GetAxis("Vertical");
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    void ProcessTranslation()
+    {
+        horizontalThrow = Input.GetAxis("Horizontal");
+        verticalThrow = Input.GetAxis("Vertical");
 
         float xOffset = horizontalThrow * Time.deltaTime * controlSpeed;
         float yOffset = verticalThrow * Time.deltaTime * controlSpeed;
@@ -16,10 +34,19 @@ public class PlayerControls : MonoBehaviour
         float newXPos = transform.localPosition.x + xOffset;
         float newYPos = transform.localPosition.y + yOffset;
 
-        float clampedXPos = Mathf.Clamp(newXPos, - xRange, xRange);
-        float clampedYPos = Mathf.Clamp(newYPos, -yRange+2, yRange+1);
+        float clampedXPos = Mathf.Clamp(newXPos, -xRange, xRange);
+        float clampedYPos = Mathf.Clamp(newYPos, -yRange + 2, yRange + 1);
 
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
-
+    }
+    void ProcessRotation()
+    {
+        //X axis Rotation
+        float pitch = (transform.localPosition.y * positionPitchFactor) + (verticalThrow * controlPitchFactor);
+        //Y axis Rotation
+        float yaw = transform.localPosition.x * positionYawFactor;
+        //Z axis Rotation
+        float roll = (transform.localPosition.x* positionRollFactor)+(horizontalThrow*controlRollFactor);
+        transform.localRotation = Quaternion.Euler(pitch,yaw,roll);
     }
 }
